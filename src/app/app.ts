0c3ed1f6d59a8524dd73cf63edc291c94b5c5d47
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-03-28 11:53:43 +0100

    build basic layout and  directory structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainHeader } from './layout/main-header/main-header';
+import { Footer } from './layout/footer/footer';
+import { MainContent } from './layout/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainHeader,Footer, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
