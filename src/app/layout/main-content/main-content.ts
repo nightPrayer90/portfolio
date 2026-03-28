d1bf727b12911cbc5aed4ca1c5613810c2db590f
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-03-28 12:34:19 +0100

    add basic components for app layout

--- src/app/layout/main-content/main-content.ts
+++ src/app/layout/main-content/main-content.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Hero } from '../../shared/components/hero/hero';
+import { WhyMe } from '../../shared/components/why-me/why-me';
+import { MySkills } from '../../shared/components/my-skills/my-skills';
+import { MyProjects } from '../../shared/components/my-projects/my-projects';
+import { Contact } from '../../shared/components/contact/contact';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [Hero, WhyMe, MySkills, MyProjects, Contact],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss',
 })
---
+++ src/app/shared/components/my-projects/my-projects.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-projects',
+  imports: [],
+  templateUrl: './my-projects.html',
+  styleUrl: './my-projects.scss',
+})
+export class MyProjects {}
