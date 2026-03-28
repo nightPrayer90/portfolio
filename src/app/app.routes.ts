e31b3b497729fe1a63dd288ecc70451f0689ea74
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-03-28 12:04:35 +0100

    imprint routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainContent } from './layout/main-content/main-content';
+import { Imprint } from './layout/imprint/imprint';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainContent
+    },
+    {
+        path: 'imprint',
+        component: Imprint
+    },
+    {
+        path:'**',
+        redirectTo:""
+    }
+];
