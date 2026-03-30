ca0382e749937ac5b99e8964068205e3e3578658
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-03-30 11:46:30 +0200

    import fonts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainHeader } from './layout/main-header/main-header';
 import { Footer } from './layout/footer/footer';
-import { MainContent } from './layout/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainHeader,Footer, MainContent],
+  imports: [RouterOutlet, MainHeader,Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
