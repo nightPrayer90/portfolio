2bbe8bd04bcea6a3a3d9eb8f285602ba7bee8e73
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-04-08 10:30:44 +0200

    add teamplayer component

---
+++ src/app/shared/components/main-content/teamplayer/teamplayer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teamplayer',
+  imports: [],
+  templateUrl: './teamplayer.html',
+  styleUrl: './teamplayer.scss',
+})
+export class Teamplayer {}
