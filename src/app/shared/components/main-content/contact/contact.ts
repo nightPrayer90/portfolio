4f8885f04778455ef6c61fd2bb646cfc2680a497
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-04-08 21:23:49 +0200

    add contact formular

--- src/app/shared/components/main-content/contact/contact.ts
+++ src/app/shared/components/main-content/contact/contact.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+  userform = new FormGroup({
+    yourName: new FormControl('', { validators: [Validators.required, Validators.minLength(3)] }),
+    yourMessage: new FormControl('', { validators: [Validators.required, Validators.minLength(5)] }),
+    yourEmail: new FormControl('', { validators: [Validators.required, Validators.email] }),
+  });
+
+  // submit btn
+  formSubmit() {
+    console.log(this.userform.value);
+    this.formReset();
+  }
+
+  formReset() {
+    this.userform.markAsUntouched();
+    this.userform.reset();
+  }
+}
