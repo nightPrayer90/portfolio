79fcccfd84e4f07d321b00cfc7e4ce289d58c69c
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-04-09 10:59:05 +0200

    add scss for language btn

--- src/app/layout/main-header/main-header.ts
+++ src/app/layout/main-header/main-header.ts
@@ -1,17 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-main-header',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, CommonModule],
   templateUrl: './main-header.html',
   styleUrl: './main-header.scss',
 })
 export class MainHeader {
-
   private translate = inject(TranslateService);
 
+  currentLang: string | null = '';
+
+  ngOnInit(): void {
+    this.currentLang = this.translate.getCurrentLang() || this.translate.getFallbackLang();
+  }
+
   switchLang(lang: string): void {
+    this.currentLang = lang;
     this.translate.use(lang);
+  }
+
+  getLangClass(lang: string): string {
+    return this.currentLang === lang ? 'language-container__button--active' : '';
   }
 }
