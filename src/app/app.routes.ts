a9fb9b1e39763511e1543ec43015d77c25994a6d
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-04-09 09:21:49 +0200

    add privacy-policy component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainContent } from './shared/components/main-content/main-content';
 import { Imprint } from './shared/components/imprint/imprint';
+import { PrivacyPolicy } from './shared/components/privacy-policy/privacy-policy';
 
 
 export const routes: Routes = [
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'imprint',
         component: Imprint
+    },
+    {
+        path: 'privacy-policy',
+        component: PrivacyPolicy
     },
     {
         path:'**',
