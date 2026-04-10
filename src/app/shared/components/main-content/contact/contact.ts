6fa0250c77157706c6e22274eb8d84902eb99c52
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2026-04-10 10:34:43 +0200

    add private policy and fix links

--- src/app/shared/components/main-content/contact/contact.ts
+++ src/app/shared/components/main-content/contact/contact.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
